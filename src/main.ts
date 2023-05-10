c94fb4efd20e4efa69dbac9a1e2ba355e994f705
liujiyou <>
2023-05-10 14:28:10 +0800

    feat: logo调整

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import {router,setupRouter} from '@/router'
-import { setupRouterGuard } from '@/router/guard';
+import { router, setupRouter } from "@/router";
+import { setupRouterGuard } from "@/router/guard";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-async function bootstrap(){
-    const app = createApp(App);
-    
-    // 配置路由
-    setupRouter(app);
-    // 路由守卫
-    setupRouterGuard(router)
-    
-    app.mount("#app");
+async function bootstrap() {
+  const app = createApp(App);
+
+  // 配置路由
+  setupRouter(app);
+  // 路由守卫
+  setupRouterGuard(router);
+
+  app.mount("#app");
 }
 
-bootstrap()
+bootstrap();
--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -1,4 +1,4 @@
 
-export function setupRouterGuard(){
+export function setupRouterGuard(router){
     
 }
