e532518f8aeee199f491150aa632e1fcd5998197
liujiyou <>
2023-05-23 13:13:27 +0800

    feat: 新增tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import router from './router';
 import ElementPlus from 'element-plus';
 import App from './App.vue';
 
+// 引入重置样式
+import './styles/reset.scss';
+// 导入公共样式
+import './styles/index.scss';
+// 一定要在main.ts中导入tailwind.css,防止vite每次hmr都会请求src/styles/index.scc整体css文件导致热更新慢的问题
+import './styles/tailwind.css';
 import 'element-plus/dist/index.css';
 
 async function bootstrap() {
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -13,9 +13,26 @@
               <h2 class="outline-none">你好</h2>
             </Motion>
             <el-form ref="ruleFormRef" :model="ruleForm" :rules="loginRules" size="large">
-              <el-form-item :rules="[{ required: true, message: '请输入账号', trigger: 'blur' }]" prop="username">
-                <el-input clearable v-model="ruleForm.username" placeholder="账号" />
-              </el-form-item>
+              <Motion :delay="100">
+                <el-form-item :rules="[{ required: true, message: '请输入账号', trigger: 'blur' }]" prop="username">
+                  <el-input clearable v-model="ruleForm.username" placeholder="账号" />
+                </el-form-item>
+              </Motion>
+              <Motion :delay="150">
+                <el-form-item :rules="[{ required: true, message: '请输入密码', trigger: 'blur' }]" prop="password">
+                  <el-input show-password clearable v-model="ruleForm.password" placeholder="密码" />
+                </el-form-item>
+              </Motion>
+              <Motion :delay="250">
+                <el-button
+                  class="w-full mt-4"
+                  size="default"
+                  type="primary"
+                  :loading="loading"
+                  @click="onLogin(ruleFormRef)">
+                  登录
+                </el-button>
+              </Motion>
             </el-form>
           </div>
         </div>
@@ -25,18 +42,34 @@
 </template>
 
 <script lang="ts" setup>
+import type { FormInstance } from 'element-plus';
 import bg from '@/assets/login/bg.png';
 import illustration from '@/assets/login/illustration.svg';
 import avatar from '@/assets/login/avatar.svg';
 import Motion from './utils/motion';
 import { loginRules } from './utils/rule';
 
-import { toRaw, reactive } from 'vue';
+import { toRaw, reactive, ref } from 'vue';
 
+const loading = ref(false);
+const ruleFormRef = ref<FormInstance>();
 const ruleForm = reactive({
   username: 'admin',
   password: 'admin123',
 });
+
+const onLogin = async (formEl: FormInstance | undefined) => {
+  loading.value = true;
+  if (!formEl) return;
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.info('🐕‍🦺登录成功', valid);
+    } else {
+      loading.value = false;
+      return fields;
+    }
+  });
+};
 </script>
 
 <style scoped lang="scss">
@@ -68,7 +101,55 @@ const ruleForm = reactive({
     text-align: center;
     .login-form {
       width: 360px;
+      .avatar {
+        width: 350px;
+        height: 80px;
+      }
+      h2 {
+        text-transform: uppercase;
+        margin: 15px 0;
+        color: #999;
+        font: bold 200% Consolas, Monaco, monospace;
+      }
     }
+  }
+}
+
+@media screen and (max-width: 1180px) {
+  .login-container {
+    grid-gap: 9rem;
+  }
+  .login-form {
+    width: 290px;
+  }
+  .login-form h2 {
+    font-size: 2.4rem;
+    margin: 8px 0;
+  }
+  .img img {
+    width: 360px;
+  }
+  .avatar {
+    width: 280px;
+    height: 80px;
+  }
+}
+
+@media screen and (max-width: 968px) {
+  .wave {
+    display: none;
+  }
+
+  .img {
+    display: none;
+  }
+
+  .login-container {
+    grid-template-columns: 1fr;
+  }
+
+  .login-box {
+    justify-content: center;
   }
 }
 </style>
