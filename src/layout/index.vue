c6d0072d44cdaf1033afe8fe2a5a6315d7b70e35
liujiyou <>
2023-05-26 16:21:55 +0800

    feat: 添加error页面

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div :class="['main-container']">
-    <div>
+  <div :class="['app-wrapper']">
+    <!-- <div v-show="set.device==='mobile' && set.sidebar.opened && layout.includes('vertical')" class="app-mask"></div> -->
+    <div :class="['main-container']">
       <layout-header />
       <!-- 主体内容 -->
       <app-main />
@@ -11,6 +12,17 @@
 <script setup lang="ts">
 import { defineComponent, h } from 'vue';
 import appMain from './components/AppMain.vue';
+
+// function setTheme(layoutModel:string){
+//   window.document.body.setAttribute("layout",layoutModel);
+//   $storage.layout = {
+//     layout:`${layoutModel}`,
+//     theme:$storage.layout?.theme,
+//     darkMode:$storage.layout?.darkMode,
+//     sidebarStatus:$storage.layout?.sidebarStatus,
+//     epThemeColor:$storage.layout?.epThemeColor
+//   }
+// }
 
 const layoutHeader = defineComponent({
   render() {
@@ -28,4 +40,23 @@ const layoutHeader = defineComponent({
 });
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@mixin clearfix {
+  &::after {
+    content:"";
+    display: table;
+    clear:both
+  }
+}
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  &.mobile.openSidebar {
+    position: fixed;
+    top: 0;
+  }
+}
+</style>
---
+++ src/views/error/404.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import noExist from "@/assets/status/404.svg"
+
+defineOptions({name:"404"})
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-[640px]">
+    <noExist/>
+    <div class="m1-l2"></div>
+  </div>
+</template>
